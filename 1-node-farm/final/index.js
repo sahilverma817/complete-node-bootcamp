564dece78eb14362f2b158d8a9480146dddaa718
Sahil Verma <sahilverma817@gmail.com>
2021-12-18 11:59:39 +0530

    Comments added

--- 1-node-farm/final/index.js
+++ 1-node-farm/final/index.js
@@ -33,24 +33,14 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////////
 // SERVER
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  'utf-8'
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
-);
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  'utf-8'
-);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
-console.log(slugs);
+
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
@@ -61,9 +51,7 @@ const server = http.createServer((req, res) => {
       'Content-type': 'text/html',
     });
 
-    const cardsHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join('');
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
 
@@ -104,4 +92,12 @@ In package.json file, we have *,^,~ in front of version of different packages.
 ~ => Only for patch updates, like bugs and all.
 ^ => For patch and minor updates.
 * => For latest version updates.
+*/
+
+
+/*
+//////////////////////////////////////////
+How other import works.
+==>      const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+//////////////////////////////////////////
 */
